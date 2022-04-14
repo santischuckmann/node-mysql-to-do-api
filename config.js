246dcfe61ac640db74d5ca296d5076bfc131b387
Santiago <schuckmannsantiago@gmail.com>
2022-04-14 00:35:45 -0300

    password now available to everyone

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 module.exports = {
   database: {
     username: "bfc3d3e61d68f0",
-    password: process.env.DATABASE_PASSWORD,
+    password: "6598fdfb",
     database: "heroku_f0a70063e0be342",
     host: "us-cdbr-east-05.cleardb.net"
   }
