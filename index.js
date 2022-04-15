eaafa39c4a95d8ee784780ba124ba1c90a87fa50
Santiago <schuckmannsantiago@gmail.com>
2022-04-15 11:42:06 -0300

    cors again

--- index.js
+++ index.js
@@ -11,9 +11,10 @@ const apiRouter = require('./routes/api')
 app.use('/api', apiRouter);
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Credentials', true);
   next();
 });
 
