3159c798e4fc423168d48babaeac0656dd0d8d63
Santiago <schuckmannsantiago@gmail.com>
2022-04-16 23:17:29 -0300

    how bout now

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT || 3001;
 app.use(express.json());
 
 app.use(cors({
-  origin: 'http://localhost:3000/'
+  origin: 'http://localhost:3'
 }))
 
 const apiRouter = require('./routes/api')
